bc94590f429dc4d5dbb27ded497c662190747f7a
Arckangel <benjamin.caroux@gmail.com>
2017-05-26 12:03:10 +0200

    debut de projet

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="style/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style/css/sass/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  </head>
+  <script src="https://use.fontawesome.com/6f5744b2fb.js"></script>
+
+  <body>
+
+  </body>
+
+</html>
